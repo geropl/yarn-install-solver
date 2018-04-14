518557244998a7fcc607f72a3989fa2c47e53f7a
geropl <gero.posmyk-leinemann@typefox.io>
2018-04-14 02:41:02 +0000

    Use theia as example

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { DependencySolver } from "./dependency-solver";
 import { DependencyResolver } from "./dependency-resolver";
-import { PackageVersion } from "./package-version";
+import { FixPackageVersion } from "./package-version";
+import { NpmInfoProvider } from "./npm-info-provider";
+import { SemVer } from "semver";
 
 const run = async () => {
     try {
         console.log("Start.");
+
+        const npmInfoProvider = new NpmInfoProvider();
+        const rootDeps = await npmInfoProvider.getDependenciesFor(new FixPackageVersion('theia', new SemVer('2.1.2')));
+
         console.log("Resolving dependencies...");
 
         let resolvedDependencies;
         {
             const depResolver = new DependencyResolver();
-            resolvedDependencies = await depResolver.resolveDependencies([PackageVersion.create('semver', '^5.0.0')]);
+            resolvedDependencies = await depResolver.resolveDependencies(rootDeps.dependencies);
         }
 
         const depSolver = new DependencySolver();
